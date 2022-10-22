30bcac0bb8bbf968a8d41319932177bc63fa61c5
Milton Ouma <ouma.nyang1@gmail.com>
2022-10-22 16:58:19 +0300

    Updates

---
+++ precache-manifest.0dfb8ab5b2464bb8be72d5e26b4a7fee.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "992b6a9ab46c73745dba5e9f797a2e7a",
+    "url": "/poverty-line/index.html"
+  },
+  {
+    "revision": "f6a20ba28ddfab6e8e4c",
+    "url": "/poverty-line/static/css/2.a779dd76.chunk.css"
+  },
+  {
+    "revision": "1ab621178bd6b37f6632",
+    "url": "/poverty-line/static/css/main.070ea7ba.chunk.css"
+  },
+  {
+    "revision": "f6a20ba28ddfab6e8e4c",
+    "url": "/poverty-line/static/js/2.aca2ab64.chunk.js"
+  },
+  {
+    "revision": "caa90a56c02f1c374365bab2e8bfbcc4",
+    "url": "/poverty-line/static/js/2.aca2ab64.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ab621178bd6b37f6632",
+    "url": "/poverty-line/static/js/main.b5cce9a6.chunk.js"
+  },
+  {
+    "revision": "5b18647ffb1b9cc45bc8",
+    "url": "/poverty-line/static/js/runtime-main.ebddc8bf.js"
+  }
+]);
